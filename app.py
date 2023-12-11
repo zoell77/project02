b9bf3ed609ee5d94599e726749a9b9b26b1c5e00
zullarfn <NamaEmail>
2023-12-11 23:20:41 +0700

    first commit

--- app.py
+++ app.py
@@ -11,13 +11,22 @@ from pymongo import MongoClient
 import requests
 from datetime import datetime
 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 app = Flask(__name__)
 
-password = 'zullarfn123'
-cxn_str = f'mongodb+srv://zullarfn:{password}@cluster0.nhlkqvs.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(cxn_str)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-db = client.dbsparta_plus_week2
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
 
 @app.route('/')
 def main():
